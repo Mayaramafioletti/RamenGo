7cbe8de0571edc87d90bbe4177327d71803d6282
Mayara <mayaracardinot1999@gmail.com>
2024-06-03 19:25:03 -0300

    feat: responsividade para mobile

--- app.js
+++ app.js
@@ -1,173 +1,158 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const brothOptionsContainer = document.getElementById("broth-options");
-  const proteinOptionsContainer = document.getElementById("protein-options");
-  const placeOrderButton = document.getElementById("place-order");
-  const orderStatusContainer = document.getElementById("order-status");
-  const successScreen = document.getElementById("success-section");
-  const orderImage = document.getElementById("success-image");
-  const orderDescription = document.getElementById("order-details");
-  const newOrderButton = document.getElementById("new-order-button");
-  const header = document.querySelector("header");
-  const main = document.querySelector("main");
-  const orderButton = document.querySelector(".order-button");
+    const brothOptionsContainer = document.getElementById("broth-options");
+    const proteinOptionsContainer = document.getElementById("protein-options");
+    const placeOrderButton = document.getElementById("place-order");
+    const orderStatusContainer = document.getElementById("order-status");
+    const successScreen = document.getElementById("success-section");
+    const orderImage = document.getElementById("success-image");
+    const orderDescription = document.getElementById("order-details");
+    const header = document.querySelector("header");
+    const main = document.querySelector("main");
+    const orderButton = document.querySelector(".order-button");
+    const newOrderButton = document.getElementById("new-order-button");
+    const brothDots = document.getElementById("broth-dots");
+    const proteinDots = document.getElementById("protein-dots");
 
-  let selectedBroth = null;
-  let selectedProtein = null;
+    let selectedBroth = null;
+    let selectedProtein = null;
 
-  const broths = [
-    {
-      id: "1",
-      imageInactive: "https://tech.redventures.com.br/icons/salt/inactive.svg",
-      imageActive: "https://tech.redventures.com.br/icons/salt/active.svg",
-      name: "Salt",
-      description: "Simple like the seawater, nothing more",
-      price: 10,
-    },
-    {
-      id: "2",
-      imageInactive: "https://tech.redventures.com.br/icons/shoyu/inactive.svg",
-      imageActive: "https://tech.redventures.com.br/icons/shoyu/active.svg",
-      name: "Shoyu",
-      description: "The good old and traditional soy sauce",
-      price: 10,
-    },
-    {
-      id: "3",
-      imageInactive: "https://tech.redventures.com.br/icons/miso/inactive.svg",
-      imageActive: "https://tech.redventures.com.br/icons/miso/active.svg",
-      name: "Miso",
-      description: "Paste made of fermented soybeans",
-      price: 12,
-    },
-  ];
-
-  const proteins = [
-    {
-      id: "1",
-      imageInactive: "https://tech.redventures.com.br/icons/pork/inactive.svg",
-      imageActive: "https://tech.redventures.com.br/icons/pork/active.svg",
-      name: "Chasu",
-      description:
-        "A sliced flavourful pork meat with a selection of season vegetables.",
-      price: 10,
-    },
-    {
-      id: "2",
-      imageInactive: "https://tech.redventures.com.br/icons/yasai/inactive.svg",
-      imageActive: "https://tech.redventures.com.br/icons/yasai/active.svg",
-      name: "Yasai Vegetarian",
-      description:
-        "A delicious vegetarian lamen with a selection of season vegetables.",
-      price: 10,
-    },
-    {
-      id: "3",
-      imageInactive:
-        "https://tech.redventures.com.br/icons/chicken/inactive.svg",
-      imageActive: "https://tech.redventures.com.br/icons/chicken/active.svg",
-      name: "Karaague",
-      description:
-        "Three units of fried chicken, moyashi, ajitama egg and other vegetables.",
-      price: 12,
-    },
-  ];
-
-  function createOptionElement(item, type) {
-    const option = document.createElement("div");
-    option.classList.add("option");
-    option.innerHTML = `
-      <img src="${item.imageInactive}" alt="${item.name}" />
-      <p>${item.name}</p>
-      <p>${item.description}</p>
-      <p>US$ ${item.price}</p>
-    `;
-    option.addEventListener("click", () => {
-      document.querySelectorAll(`#${type}-options .option`).forEach((el) => {
-        el.classList.remove("selected");
-        el.querySelector("img").src = el.dataset.imageInactive;
-      });
-
-      option.classList.add("selected");
-      option.querySelector("img").src = item.imageActive;
-
-      if (type === "broth") {
-        selectedBroth = item.id;
-      } else {
-        selectedProtein = item.id;
-      }
-      updateButtonState();
-    });
-
-    option.dataset.imageInactive = item.imageInactive;
-    option.dataset.imageActive = item.imageActive;
-
-    return option;
-  }
-
-  function updateButtonState() {
-    placeOrderButton.disabled = !(selectedBroth && selectedProtein);
-  }
-
-  function loadOptions(data, container, type) {
-    data.forEach((item) => {
-      const optionElement = createOptionElement(item, type);
-      container.appendChild(optionElement);
-    });
-  }
-
-  async function placeOrder() {
-    try {
-      // Simulando a colocação bem-sucedida do pedido
-      const data = {
-        image: "https://tech.redventures.com.br/ramen-success.png", // Imagem mock
-        description: `Broth: ${selectedBroth}, Protein: ${selectedProtein}`,
-      };
-      showSuccessScreen(data.image, data.description);
-    } catch (error) {
-      console.error("Error placing order:", error);
-      orderStatusContainer.innerHTML =
-        "<p>There was an error placing your order. Please try again.</p>";
+    async function loadOptionsFromAPI(endpoint, container, type) {
+        try {
+            const response = await fetch(endpoint, {
+                headers: { 'x-api-key': 'ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf' }
+            });
+            const data = await response.json();
+            loadOptions(data, container, type);
+            createDots(container, type === 'broth' ? brothDots : proteinDots);
+            updateDots(container, type === 'broth' ? brothDots : proteinDots);
+        } catch (error) {
+            console.error(`Error loading ${type} options:`, error);
+        }
     }
-  }
 
-  function showSuccessScreen(image, description) {
-    orderImage.src = image;
-    orderDescription.textContent = description;
-    header.classList.add("hidden");
-    main.classList.add("hidden");
-    successScreen.classList.remove("hidden");
-    successScreen.style.display = "flex"; // Garantir que seja exibido
-  }
+    function createOptionElement(item, type) {
+        const option = document.createElement("div");
+        option.classList.add("option");
+        option.innerHTML = `
+            <img src="${item.imageInactive}" alt="${item.name}" />
+            <p>${item.name}</p>
+            <p>${item.description}</p>
+            <p>US$ ${item.price}</p>
+        `;
+        option.addEventListener("click", () => {
+            document.querySelectorAll(`#${type}-options .option`).forEach((el) => {
+                el.classList.remove("selected");
+                el.querySelector("img").src = el.dataset.imageInactive;
+            });
 
-  function startNewOrder() {
-    header.classList.remove("hidden");
-    main.classList.remove("hidden");
-    successScreen.classList.add("hidden");
-    successScreen.style.display = "none"; // Ocultar novamente
+            option.classList.add("selected");
+            option.querySelector("img").src = item.imageActive;
+            if (type === "broth") {
+                selectedBroth = item.id;
+            } else {
+                selectedProtein = item.id;
+            }
+            updateButtonState();
+        });
 
-    // Resetar seleção e estado do botão
-    selectedBroth = null;
-    selectedProtein = null;
-    updateButtonState();
+        option.dataset.imageInactive = item.imageInactive;
+        option.dataset.imageActive = item.imageActive;
+        return option;
+    }
 
-    // Limpar seleções visuais
-    document.querySelectorAll(".option").forEach((el) => {
-      el.classList.remove("selected");
-      el.querySelector("img").src = el.dataset.imageInactive;
+    function updateButtonState() {
+        placeOrderButton.disabled = !(selectedBroth && selectedProtein);
+    }
+
+    function loadOptions(data, container, type) {
+        data.forEach((item) => {
+            const optionElement = createOptionElement(item, type);
+            container.appendChild(optionElement);
+        });
+    }
+
+    async function placeOrder() {
+        try {
+            const response = await fetch("https://api.tech.redventures.com.br/orders", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-api-key': 'ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf'
+                },
+                body: JSON.stringify({
+                    brothId: selectedBroth,
+                    proteinId: selectedProtein
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const data = await response.json();
+            showSuccessScreen(data.image, data.description);
+        } catch (error) {
+            console.error("Error placing order:", error);
+            orderStatusContainer.innerHTML = "<p>There was an error placing your order. Please try again.</p>";
+        }
+    }
+
+    function showSuccessScreen(image, description) {
+        orderImage.src = image;
+        orderDescription.textContent = description;
+        header.classList.add("hidden");
+        main.classList.add("hidden");
+        successScreen.classList.remove("hidden");
+        successScreen.style.display = "flex";
+        successScreen.scrollIntoView({ behavior: "smooth" });
+    }
+
+    function startNewOrder() {
+        header.classList.remove("hidden");
+        main.classList.remove("hidden");
+        successScreen.classList.add("hidden");
+        successScreen.style.display = "none";
+        selectedBroth = null;
+        selectedProtein = null;
+        updateButtonState();
+        document.querySelectorAll(".option").forEach((el) => {
+            el.classList.remove("selected");
+            el.querySelector("img").src = el.dataset.imageInactive;
+        });
+        header.scrollIntoView({ behavior: "smooth" });
+    }
+
+    loadOptionsFromAPI("https://api.tech.redventures.com.br/broths", brothOptionsContainer, "broth");
+    loadOptionsFromAPI("https://api.tech.redventures.com.br/proteins", proteinOptionsContainer, "protein");
+
+    placeOrderButton.addEventListener("click", placeOrder);
+    newOrderButton.addEventListener("click", startNewOrder);
+    orderButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelector("main").scrollIntoView({ behavior: "smooth" });
     });
-  }
 
-  loadOptions(broths, brothOptionsContainer, "broth");
-  loadOptions(proteins, proteinOptionsContainer, "protein");
+    function createDots(carousel, dotsContainer) {
+        dotsContainer.innerHTML = '';
+        const items = carousel.children;
+        for (let i = 0; i < items.length; i++) {
+            const dot = document.createElement('div');
+            dot.classList.add('dot');
+            if (i === 0) dot.classList.add('active');
+            dot.addEventListener('click', () => {
+                carousel.scrollTo({ left: items[i].offsetLeft, behavior: 'smooth' });
+            });
+            dotsContainer.appendChild(dot);
+        }
+    }
 
-  placeOrderButton.addEventListener("click", placeOrder);
-  newOrderButton.addEventListener("click", startNewOrder);
-
-  // Smooth scroll to main section on "ORDER NOW" click
-  orderButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    document.querySelector("main").scrollIntoView({
-      behavior: "smooth",
-    });
-  });
+    function updateDots(carousel, dotsContainer) {
+        const items = carousel.children;
+        carousel.addEventListener('scroll', () => {
+            let index = Math.round(carousel.scrollLeft / items[0].offsetWidth);
+            dotsContainer.querySelectorAll('.dot').forEach((dot, i) => {
+                dot.classList.toggle('active', i === index);
+            });
+        });
+    }
 });
